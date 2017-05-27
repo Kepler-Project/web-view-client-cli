161d65f44818bc2b7e177a4d25c6af9f5f8d6587
crawl <danielcrawl@gmail.com>
2017-05-26 18:22:04 -0700

    moving fields to _fields dict

--- app/webview/run.py
+++ app/webview/run.py
@@ -4,53 +4,66 @@ Kepler Web-View Run
 
 """
 
+import json
 import requests
 from requests.auth import HTTPBasicAuth
 
 class Run:
 
     def __init__(self, url='https://localhost:9443/kepler', username=None, 
-        password=None, response=None, id=None):
+        password=None, response=None, id=None, fields=None):
 
-        self._error = None
         self._is_running = None
-        self._password = password
         self._outputs = None
-        self._run_id = id
+        self._password = password
         self._success = False
         self._url = url
         self._username = username
+
+        if fields is not None:
+            self._fields = fields
+            print(fields)
+        else:
+            self._fields = {}
+
+        if id is not None:
+            self._fields['id'] = id
 
         if response is not None:
             
             print("DEBUG: {}".format(response.text))
 
             if response.text == 'Unauthorized':
-                self._error = response.text
+                self._fields['error'] = response.text
             else:
                 response_json = response.json()
     
                 if response.status_code != requests.codes.ok:
                     if 'error' in response_json:
-                        self._error = response_json['error']
+                        self._fields['error'] = response_json['error']
                 else:
                     self._success = True
     
-                    if 'runid' in response_json:
-                        self._run_id = response_json['runid']
+                    if 'id' in response_json:
+                        self._fields['id'] = response_json['id']
     
                     if 'responses' in response_json:
                         self._is_running = False
                         self._outputs = response_json['responses']
 
+    def __str__(self):
+        return str(self._fields)
+
     # get error message if failure, otherwise None
     def error(self):
         # see if error already set or no longer running
-        if self._error is not None or self._is_running is False:
-            return self._error
-        
-        self.status();
-        return self._error
+        if 'error' not in self._fields and self._is_running is True:
+            self.status()
+
+        if 'error' in self._fields:
+            return self._fields['error']
+        else:
+            return None
 
     # wait until run finished
     # returns True if success, False otherwise
@@ -59,7 +72,7 @@ class Run:
 
     # get the run id. returns None if provenance not enabled.
     def id(self):
-        return self._run_id
+        return self._fields['id']
     
     # get if the run is still running.
     def is_running(self):
@@ -72,7 +85,8 @@ class Run:
     # get the run status
     def status(self, outputs=False):
         response = requests.get("{}/runs/{}?outputs={}".format(self._url, 
-            self._run_id, str(outputs).lower()), auth=HTTPBasicAuth(self._username, self._password),
+            self._fields['id'], str(outputs).lower()), 
+            auth=HTTPBasicAuth(self._username, self._password),
             #FIXME verify
             verify=False)
         
@@ -83,11 +97,11 @@ class Run:
 
         # error handling first
         if 'error' in self._status:
-            self._error = self._status['error']
+            self._fields['error'] = self._status['error']
 
         if response.status_code != requests.codes.ok:
             return
-            #raise Exception(self._error)
+            #raise Exception(self._fields['error'])
       
         self._is_running = self._status['status'] == 'running'
                     
